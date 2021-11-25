3e2af172d018958e9d73973c4a6571d4e3417c45
Steven Harley <smharley@gmail.com>
2021-11-24 17:27:23 -0800

    Add colors for pins

--- src/components/lighthouses/Pins.js
+++ src/components/lighthouses/Pins.js
@@ -1,9 +1,10 @@
+import * as React from 'react';
 import { Marker } from 'react-map-gl'
 
 const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3 c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9 C20.1,15.8,20.2,15.8,20.2,15.7z`;
 const SIZE = 20;
 
-const Pins = (props) => {
+function Pins(props) {
     const {data, onClick} = props;
   
     return data.map((lighthouse, index) => {
@@ -17,17 +18,17 @@ const Pins = (props) => {
             viewBox="0 0 24 24"
             style={{
               cursor: 'pointer',
-              fill: '#d00',
+              fill: lighthouse.properties.fill,
               stroke: 'none',
               transform: `translate(${-SIZE / 2}px,${-SIZE}px)`
             }}
             onClick={() => onClick(lighthouse)}
           >
-            <path d={ICON} />
+            <path d={ICON} stroke="#000" strokeOpacity="0.7" strokeWidth="2" />
           </svg>
         </Marker>
       )
     });
   }
 
-export default Pins;
+export default React.memo(Pins);
--- src/pages/Lighthouses.js
+++ src/pages/Lighthouses.js
@@ -1,101 +1,104 @@
-import React, { useEffect, useState } from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import NotFound from './NotFound';
-import Pins from '../components/lighthouses/Pins';
-import Info from '../components/lighthouses/Info';
-import Prismic from '@prismicio/client';
-import ReactMapGl, { Popup } from 'react-map-gl';
-import mapboxgl from 'mapbox-gl';
-import { DefaultLayout } from '../components';
-import { RichText } from 'prismic-reactjs';
-import { client } from '../utils/prismicHelpers';
+    import React, { useEffect, useState } from 'react';
+    import 'mapbox-gl/dist/mapbox-gl.css';
+    import NotFound from './NotFound';
+    import Pins from '../components/lighthouses/Pins';
+    import Info from '../components/lighthouses/Info';
+    import Prismic from '@prismicio/client';
+    import ReactMapGl, { Popup } from 'react-map-gl';
+    import mapboxgl from 'mapbox-gl';
+    import { DefaultLayout } from '../components';
+    import { RichText } from 'prismic-reactjs';
+    import { client } from '../utils/prismicHelpers';
 
-// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
-const mapboxToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+    // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+    mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+    const mapboxToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
-const Lighthouses = () => {
-  const [prismicData, setPrismicData] = useState({ lighthouses: null });
-  const [notFound, toggleNotFound] = useState(false);
-  const [popupInfo, setPopupInfo] = useState(null);
-  const [viewport, setViewport] = useState({
-    latitude: 44.912879,
-    longitude: -84.7586996,
-    width: '100vw',
-    height: '100vh',
-    zoom: 6
-  });
-
-  useEffect(() => {
-    const fetchPrismicData = async () => {
-      try {
-        const lighthouses = await client.query(
-          Prismic.Predicates.at('document.type', 'lighthouse'),
-          { orderings: '[my.post.date desc]' }
-        );
-  
-        if (lighthouses) {
-          setPrismicData({ lighthouses: lighthouses.results });
-        } else {
-          console.warn('Blog Home document was not found. Make sure it exists in your Prismic repository');
-          toggleNotFound(true);
-        }
-      } catch (error) {
-        console.error(error);
-        toggleNotFound(true);
-      }
-    }
-
-    fetchPrismicData();
-  }, []);
-
-  if (prismicData.lighthouses) {
-    let features = [];
-    prismicData.lighthouses.map(lighthouse => {
-      const latitude = lighthouse.data.location.latitude;
-      const longitude = lighthouse.data.location.longitude;
-      const name = RichText.asText(lighthouse.data.name);
-      const description = RichText.asText(lighthouse.data.description);
-      features.push(
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [longitude, latitude]
-          },
-          "properties": {
-            "name": name,
-            "image": lighthouse.data.image,
-            "description": description
-          }
-        }
-      )
-      return features;
+    const Lighthouses = () => {
+    const [prismicData, setPrismicData] = useState({ lighthouses: null });
+    const [notFound, toggleNotFound] = useState(false);
+    const [popupInfo, setPopupInfo] = useState(null);
+    const [viewport, setViewport] = useState({
+        latitude: 44.912879,
+        longitude: -84.7586996,
+        width: '100vw',
+        height: '100vh',
+        zoom: 6
     });
 
-    return (
-      <DefaultLayout seoTitle="Lighthouse Project">
-        <ReactMapGl mapboxApiAccessToken={mapboxToken} mapStyle="mapbox://styles/mapbox/dark-v10" {...viewport} onViewportChange={setViewport}>
-          <Pins data={features} onClick={setPopupInfo} />
-          {popupInfo && (
-            <Popup
-              tipSize={5}
-              anchor="top"
-              longitude={popupInfo.geometry.coordinates[0]}
-              latitude={popupInfo.geometry.coordinates[1]}
-              closeOnClick={false}
-              onClose={setPopupInfo}
-            >
-              <Info info={popupInfo} />
-            </Popup>
-          )}
-        </ReactMapGl>
-      </DefaultLayout>
-    );
-  } else if (notFound) {
-    return <NotFound />;
-  }
-  return null;
-}
+    useEffect(() => {
+        const fetchPrismicData = async () => {
+        try {
+            const lighthouses = await client.query(
+            Prismic.Predicates.at('document.type', 'lighthouse'),
+            { orderings: '[my.post.date desc]' }
+            );
+    
+            if (lighthouses) {
+                let features = [];
+                lighthouses.results.map(lighthouse => {
+                    const latitude = lighthouse.data.location.latitude;
+                    const longitude = lighthouse.data.location.longitude;
+                    const name = RichText.asText(lighthouse.data.name);
+                    const description = RichText.asText(lighthouse.data.description);
+                    const colors = ['#f0b8b8', '#aecdc2', '#665191', '#a05195', '#88c9d4', '#d45087', '#f95d6a', '#ff7c43', '#ffa600' ];
+                    const fill = colors[Math.floor(Math.random() * colors.length)];
 
-export default Lighthouses;
+                    features.push(
+                        {
+                            "type": "Feature",
+                            "geometry": {
+                            "type": "Point",
+                            "coordinates": [longitude, latitude]
+                            },
+                            "properties": {
+                                "name": name,
+                                "image": lighthouse.data.image,
+                                "description": description,
+                                "fill": fill
+                            }
+                        }
+                    )
+                    return features;
+                })
+                setPrismicData({ lighthouses: features });
+            } else {
+                console.warn('Blog Home document was not found. Make sure it exists in your Prismic repository');
+                toggleNotFound(true);
+                }
+            } catch (error) {
+                console.error(error);
+                toggleNotFound(true);
+            }
+        }
+
+        fetchPrismicData();
+    }, []);
+
+    if (prismicData.lighthouses) {
+        return (
+        <DefaultLayout seoTitle="Lighthouse Project">
+            <ReactMapGl mapboxApiAccessToken={mapboxToken} mapStyle="mapbox://styles/mapbox/dark-v10" {...viewport} onViewportChange={setViewport}>
+            <Pins data={prismicData.lighthouses} onClick={setPopupInfo} />
+            {popupInfo && (
+                <Popup
+                tipSize={5}
+                anchor="top"
+                longitude={popupInfo.geometry.coordinates[0]}
+                latitude={popupInfo.geometry.coordinates[1]}
+                closeOnClick={false}
+                onClose={setPopupInfo}
+                >
+                <Info info={popupInfo} />
+                </Popup>
+            )}
+            </ReactMapGl>
+        </DefaultLayout>
+        );
+    } else if (notFound) {
+        return <NotFound />;
+    }
+    return null;
+    }
+
+    export default Lighthouses;
