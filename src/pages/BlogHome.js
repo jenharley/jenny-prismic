4f001f3681e93c9416d10bb7b71d080ac6d48117
Steven Harley <smharley@gmail.com>
2021-12-01 20:24:28 -0800

    Dateline, date, moment, masonry, truncation, tags

--- src/pages/BlogHome.js
+++ src/pages/BlogHome.js
@@ -6,72 +6,185 @@ import { Link } from 'react-router-dom';
 import { RichText } from 'prismic-reactjs';
 import { client } from '../utils/prismicHelpers';
 import { linkResolver } from '../prismic-configuration';
+import styled from 'styled-components';
+import moment from 'moment';
+import Masonry from '@mui/lab/Masonry';
+import { Tab } from '@mui/material';
+
+const Date = styled.span`
+    color: #ff2c54;
+    font-size: 0.75rem;
+    font-weight: 700;
+    letter-spacing: -.02em;
+    text-transform: uppercase;
+    white-space: nowrap;
+`;
+
+const Dateline = styled.div`
+    align-items: baseline;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1rem;
+
+    div {
+        align-items: baseline;
+        display: flex;
+        column-gap: 0.5rem;
+    }
+`;
+
+const ReadMore = styled(Link)`
+    color: #ff2c54;
+    font-size: 0.75rem;
+    font-weight: 700;
+    letter-spacing: -.02em;
+    text-decoration: none;
+    text-transform: uppercase;
+    white-space: nowrap;
+`;
+
+const Post = styled(Link)`
+    color: #111;
+    text-decoration: none;
+
+    h2 {
+        font-size: 1.625rem;
+        font-weight: 400;
+        line-height: 1.35;
+        margin-bottom: 0.5rem;
+        margin-top: 1rem;
+    }
+
+    p {
+        font-weight: 300;
+        line-height: 1.55;
+    }
+`;
+
+const PostList = styled.ul`
+    display: grid;
+    margin: 0 auto;
+    max-width: 1100px;
+`;
+
+const StyledTagList = styled.ul`
+    color: #bababa;
+    display: inline-block;
+    font-size: 0.75rem;
+    font-weight: 700;
+    text-transform: uppercase;
+`;
+
+const truncate = (string, useLength) => {
+    const LENGTH = 155;
+    const length = useLength ? useLength : LENGTH;
+
+    return string.length > length ? string.substring(0, length - 3) + `…` : string;
+};
+
+const intersperse = (array, separator) => {
+    if (array.length === 0) {
+        return [];
+    }
+
+    return array.slice(1).reduce(function(xs, x, i) {
+        return xs.concat([separator, x]);
+    }, [array[0]]);
+}
 
 const PostItem = (props) => {
-  const { post } = props;
-  const title = RichText.asText(post.data?.title);
+    const { post } = props;
+    const { data } = post;
+    const { body, publish_date, tags, title } = data;
+    const blog_title = RichText.asText(title);
+    const description = RichText.asText(body[0].primary.blog_post_body);
+    const date = moment(publish_date).format("MMM D, YYYY")
 
-  return (
-    <Link to={linkResolver(post)}>
-      <h2>{title}</h2>
-    </Link>
-  );
+    return (
+        <Post to={linkResolver(post)}>
+            <img src="http://placekitten.com/1300/1418" />
+            <h2>{blog_title}</h2>
+            <p>{truncate(description)}</p>
+            <Dateline>
+                <div>
+                    <Date>{date}</Date>
+                    {tags && <TagList tags={tags} />}
+                </div>
+                <ReadMore to={linkResolver(post)}>Read More</ReadMore> 
+            </Dateline>
+        </Post>
+    );
 };
 
 const BlogPosts = (props) => {
-  const { posts } = props;
+    const { posts } = props;
 
-  return (
-    <>
-      {posts.map((post) => (
-        <PostItem post={post} key={post.id} />
-      ))}
-    </>
-  );
+    return (
+        <PostList>
+            <Masonry columns={3} spacing={5}>
+                {posts.map((post) => (
+                    <PostItem post={post} key={post.id} />
+                ))}
+            </Masonry>
+        </PostList>
+    );
+};
+
+const TagList = (props) => {
+    const { tags } = props;
+    const array = tags.map(tag => tag.tag);
+    const tagList = array.join(", ").toString();
+
+    return (
+        <StyledTagList>
+            {truncate(tagList, 20)}
+        </StyledTagList>
+    )
 };
 
 const BlogHome = () => {
-  const [prismicData, setPrismicData] = useState({ homeDoc: null, posters: null, blog_posts: null });
-  const [notFound, toggleNotFound] = useState(false);
+    const [prismicData, setPrismicData] = useState({ homeDoc: null, posters: null, blog_posts: null });
+    const [notFound, toggleNotFound] = useState(false);
 
-  // Get the homepage and blog post documents from Prismic
-  useEffect(() => {
-    const fetchPrismicData = async () => {
-      try {
-        const blog_posts = await client.query(
-          Prismic.Predicates.at('document.type', 'blog_post'),
-          { orderings: '[my.post.date desc]' }
-        );
-  
-        if (blog_posts) {
-          setPrismicData({ blog_posts: blog_posts.results });
-        } else {
-          console.warn('Blog Home document was not found. Make sure it exists in your Prismic repository');
-          toggleNotFound(true);
+    // Get the homepage and blog post documents from Prismic
+    useEffect(() => {
+        const fetchPrismicData = async () => {
+            try {
+                const blog_posts = await client.query(
+                    Prismic.Predicates.at('document.type', 'blog_post'),
+                        { orderings: '[my.post.date desc]' }
+                );
+
+                if (blog_posts) {
+                    setPrismicData({ blog_posts: blog_posts.results });
+                } else {
+                    console.warn('Blog Home document was not found. Make sure it exists in your Prismic repository');
+                    toggleNotFound(true);
+                }
+            } catch (error) {
+                console.error(error);
+                toggleNotFound(true);
+            }
         }
-      } catch (error) {
-        console.error(error);
-        toggleNotFound(true);
-      }
+
+        fetchPrismicData();
+    }, []);
+
+    // Return the page if a document was retrieved from Prismic
+    if (prismicData.blog_posts) {
+        const blog_posts = prismicData.blog_posts;
+
+        return (
+        <DefaultLayout seoTitle={"Blog"}>
+            Blog
+            <BlogPosts posts={blog_posts} />
+        </DefaultLayout>
+        );
+    } else if (notFound) {
+        return <NotFound />;
     }
 
-    fetchPrismicData();
-  }, []);
-
-  // Return the page if a document was retrieved from Prismic
-  if (prismicData.blog_posts) {
-    const blog_posts = prismicData.blog_posts;
-
-    return (
-      <DefaultLayout seoTitle={"Blog"}>
-        Blog
-        <BlogPosts posts={blog_posts} />
-      </DefaultLayout>
-    );
-  } else if (notFound) {
-    return <NotFound />;
-  }
-  return null;
+    return null;
 }
 
 export default BlogHome;
