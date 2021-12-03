6cbe8a450a56520d3162e567bb0e12d74f265524
Steven Harley <smharley@gmail.com>
2021-12-02 19:09:44 -0800

    Lighthouse popup WIP

---
+++ src/components/lighthouses/Pin.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Marker } from 'react-map-gl';
+import { ICON, SIZE } from './Pins';
+
+function Pin(props) {
+    const { lighthouse } = props;
+    const latitude = lighthouse.geometry.coordinates[1];
+    const longitude = lighthouse.geometry.coordinates[0];
+
+    return(
+        <Marker longitude={longitude} latitude={latitude}>
+            <svg height={SIZE}
+                viewBox="0 0 24 24"
+                style={{
+                cursor: 'pointer',
+                fill: lighthouse.properties.fill,
+                stroke: 'none',
+                transform: `translate(${-SIZE / 2}px,${-SIZE}px)`
+                }}
+            >
+                <path d={ICON} stroke="#000" strokeOpacity="0.7" strokeWidth="2" />
+            </svg>
+        </Marker>
+    );
+};
+
+export default React.memo(Pin);
--- src/pages/Lighthouses.js
+++ src/pages/Lighthouses.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { ICON, SIZE } from '../components/lighthouses/Pins';
 import { respondTo } from '../utils/StyleUtil';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import NotFound from './NotFound';
 import Pins from '../components/lighthouses/Pins';
 import Info from '../components/lighthouses/Info';
 import Prismic from '@prismicio/client';
-import ReactMapGl from 'react-map-gl';
+import ReactMapGl, { Marker } from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 import { DefaultLayout } from '../components';
 import { RichText } from 'prismic-reactjs';
@@ -13,6 +14,8 @@ import { client } from '../utils/prismicHelpers';
 import styled, { css } from 'styled-components';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import Modal from '@mui/material/Modal';
+import MaxWidthContainer from '../components/MaxWidthContainer';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
@@ -75,13 +78,84 @@ const Drawer = styled.div`
 
 const LighthouseGrid = styled.div`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: 1fr;
     gap: 2rem;
     padding-top: 100px;
 
+    ${respondTo('mobile')`
+        grid-template-columns: repeat(2, 1fr);
+    `}
+
+    ${respondTo('laptop')`
+        grid-template-columns: repeat(3, 1fr);
+    `}
+
+    ${respondTo('desktop')`
+        grid-template-columns: repeat(4, 1fr);
+    `}
+
     li {
+        cursor: pointer;
         list-style: none;
     }
+`;
+
+const ModalInner = styled.div`
+    background: #fff;
+    border-radius: 10px;
+    display: grid;
+    left: 50%;
+    max-height: 100vh;
+    max-width: 480px;
+    overflow-y: auto;
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+
+    img {
+        display: block;
+        border-radius: 0 0 10px 10px;
+
+        ${respondTo('laptop', 'max', 'height')`
+            grid-column: 2;
+            grid-row: 1 / 3;
+        `}
+    }
+
+    ${respondTo('desktop')`
+        max-width: 580px;
+    `}
+
+    ${respondTo('laptop', 'max', 'height')`
+        grid-template-columns: 1fr 400px;
+        grid-template-rows: 1fr 200px;
+        max-width: 880px;
+    `}
+`;
+
+const Name = styled.h3`
+    color: #41294a;
+    font-size: 26px;
+    font-weight: 500;
+    letter-spacing: -0.01em;
+    line-height: 1.25em;
+`;
+
+const ModalMap = styled.div`
+    ${respondTo('laptop', 'max', 'height')`
+        grid-column: 1;
+        grid-row: 2;
+    `}
+`;
+
+const ModalName = styled(Name)`
+    padding: 2rem;
+
+    ${respondTo('laptop', 'max', 'height')`
+        grid-column: 1;
+        grid-row: 1;
+    `}
 `;
 
 const ViewOptions = styled.div`
@@ -97,6 +171,10 @@ const ViewOptions = styled.div`
 `;
 
 const Lighthouses = () => {
+    const [open, setOpen] = useState(false);
+    const [modalData, setModalData] = useState(null);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const [prismicData, setPrismicData] = useState({ lighthouses: null });
     const [notFound, toggleNotFound] = useState(false);
     const [popupInfo, setPopupInfo] = useState(null);
@@ -183,21 +261,34 @@ const Lighthouses = () => {
                         </Container>
                     }
                     {showGrid &&
-                        <LighthouseGrid>
-                            {prismicData.lighthouses.map(lighthouse => {
-                                const { geometry, properties } = lighthouse;
-                                const { name, image } = properties;
+                        <MaxWidthContainer>
+                            <LighthouseGrid>
+                                {prismicData.lighthouses.map(lighthouse => {
+                                    const { geometry, properties } = lighthouse;
+                                    const { name, image } = properties;
 
-                                return (
-                                    <li key={geometry.coordinates[0]}>
-                                        <img src={image.url} alt={name} />
-                                        {name}
-                                    </li>
-                                )
-                            })}
-
-                        </LighthouseGrid>
+                                    return (
+                                        <li key={geometry.coordinates[0]} onClick={() => {handleOpen(); setModalData(lighthouse);}}>
+                                            <img src={image.url} alt={name} />
+                                            <Name>{name}</Name>
+                                        </li>
+                                    )
+                                })}
+                            </LighthouseGrid>
+                        </MaxWidthContainer>
                     }
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <>
+                            {!!modalData &&
+                                <Content lighthouse={modalData} />
+                            }
+                        </>
+                    </Modal>
                 </DefaultLayout>
             </>
         );
@@ -206,5 +297,38 @@ const Lighthouses = () => {
     }
     return null;
 }
+
+const Content = (props) => {
+    const { lighthouse } = props;
+    const [viewport, setViewport] = React.useState({
+        longitude: lighthouse.geometry.coordinates[0],
+        latitude: lighthouse.geometry.coordinates[1],
+        zoom: 12
+    });
+
+    return (
+        <ModalInner>
+            <ModalName>{lighthouse.properties.name}</ModalName>
+            <ModalMap>
+                <ReactMapGl mapboxApiAccessToken={mapboxToken} mapStyle="mapbox://styles/mapbox/dark-v10" height="300px" width="100%" {...viewport} onViewportChange={setViewport}>
+                    <Marker longitude={lighthouse.geometry.coordinates[0]} latitude={lighthouse.geometry.coordinates[1]}>
+                        <svg height={SIZE}
+                            viewBox="0 0 24 24"
+                            style={{
+                            cursor: 'pointer',
+                            fill: 'green',
+                            stroke: 'none',
+                            transform: `translate(${-SIZE / 2}px,${-SIZE}px)`
+                            }}
+                        >
+                            <path d={ICON} stroke="#000" strokeOpacity="0.7" strokeWidth="2" />
+                        </svg>
+                    </Marker>
+                </ReactMapGl>
+            </ModalMap>
+            <img src={lighthouse.properties.image.url} alt={lighthouse.properties.name} />
+        </ModalInner>
+    );
+};
 
 export default Lighthouses;
