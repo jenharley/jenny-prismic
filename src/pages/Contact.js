930b8ad889699755af754e6c7d8dbf73ba8d82b2
Steven Harley <smharley@gmail.com>
2021-12-25 11:59:59 -0800

    Form design

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,6 +6,8 @@ import styled from 'styled-components';
 import { DefaultLayout } from '../components';
 import { Title } from './BlogHome';
 import { Honeypot, NetlifyForm, Recaptcha } from 'react-netlify-forms';
+import { MaxWidthContainer } from '../components/MaxWidthContainer';
+import { Grid } from '@mui/material';
 
 const Form = styled(NetlifyForm)`
     display: grid;
@@ -17,64 +19,78 @@ const Form = styled(NetlifyForm)`
 const Contact = props => {
     return (
         <DefaultLayout>
-            <Title>
-                Contact
-            </Title>
-            <Form
-                name='contact'
-                action='/thanks'
-                honeypotName='bot-field'
-                enableRecaptcha
-                onSuccess={(response, context) => {
-                    console.log('Successfully sent form data to Netlify Server');
-                    context.formRef.current.reset();
-                }}
-            >
-                {({ handleChange, success, error }) => (
-                    <>
-                        <Honeypot />
-                        <Recaptcha siteKey={'6LegXXcdAAAAACtakT-rVDrxVVTHKRhs9mE4jHKE'} invisible />
-                        {success && (
-                            <p sx={{ variant: 'alerts.success', p: 3 }}>
-                            Thanks for contacting us!
-                            </p>
-                        )}
-                        {error && (
-                            <p sx={{ variant: 'alerts.muted', p: 3 }}>
-                            Sorry, we could not reach servers. Because it only works on Netlify,
-                            our GitHub demo does not provide a response.
-                            </p>
-                        )}
-                        <div>
-                            <TextField
-                                id="name"
-                                label="Name"
-                                required
-                            />
-                        </div>
-                        <div sx={{ pt: 2 }}>
-                            <TextField
-                                id="message"
-                                label="Message"
-                                multiline
-                                required
-                                rows={4}
-                            />
-                        </div>
-                        <div sx={{ pt: 3 }}>
-                            <Button
-                                disableElevation
-                                endIcon={<SendIcon />}
-                                size="large"
-                                type="submit"
-                                variant="contained"
-                            >
-                                Send
-                            </Button>
-                        </div>
-                    </>
-                )}
-            </Form>
+            <MaxWidthContainer max={480}>
+                <Title style={{ paddingBottom: '3rem' }}>
+                    Contact
+                </Title>
+                <Form
+                    name='contact'
+                    action='/thanks'
+                    honeypotName='bot-field'
+                    enableRecaptcha
+                    onSuccess={(response, context) => {
+                        console.log('Successfully sent form data to Netlify Server');
+                        context.formRef.current.reset();
+                    }}
+                >
+                    {({ handleChange, success, error }) => (
+                        <Grid container spacing={4}>
+                            <Honeypot />
+                            <Recaptcha siteKey={'6LegXXcdAAAAACtakT-rVDrxVVTHKRhs9mE4jHKE'} invisible />
+                            {success && (
+                                <p sx={{ variant: 'alerts.success', p: 3 }}>
+                                Thanks for contacting us!
+                                </p>
+                            )}
+                            {error && (
+                                <p sx={{ variant: 'alerts.muted', p: 3 }}>
+                                Sorry, we could not reach servers. Because it only works on Netlify,
+                                our GitHub demo does not provide a response.
+                                </p>
+                            )}
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="name"
+                                    label="Name"
+                                    required
+                                    sx={{ width: '100%' }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="email"
+                                    label="Email"
+                                    type="email"
+                                    required
+                                    sx={{ width: '100%' }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    id="message"
+                                    label="Message"
+                                    multiline
+                                    required
+                                    rows={4}
+                                    sx={{ width: '100%' }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button
+                                    disableElevation
+                                    endIcon={<SendIcon />}
+                                    size="large"
+                                    sx={{ width: '100%' }}
+                                    type="submit"
+                                    variant="contained"
+                                >
+                                    Send
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    )}
+                </Form>
+            </MaxWidthContainer>
         </DefaultLayout>
     );
 };
